e5542173e7172d8d169c5b14cf66a634ffea006e
Anton Liverant <anliberant@gmail.com>
2020-10-05 10:50:48 +0300

    moving

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 let map = Array(),
+    map2 = Array(),
     helpers = Array(),
-    moveColor = 'white';
+    moveColor = 'white',
+    moveFromX,
+    moveFromY;
 function initMap() {
-    map = [
+    map2 = [
         ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
         ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
         ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+    ];
+    map = [
+        ['R', 'P', ' ', ' ', ' ', ' ', 'p', 'r'],
+        ['N', 'P', ' ', ' ', ' ', ' ', 'p', 'n'],
+        ['B', 'P', ' ', ' ', ' ', ' ', 'p', 'b'],
+        ['Q', 'P', ' ', ' ', ' ', ' ', 'p', 'q'],
+        ['K', 'P', ' ', ' ', ' ', ' ', 'p', 'k'],
+        ['B', 'P', ' ', ' ', ' ', ' ', 'p', 'b'],
+        ['N', 'P', ' ', ' ', ' ', ' ', 'p', 'n'],
+        ['R', 'P', ' ', ' ', ' ', ' ', 'p', 'r'],
     ];
 }
 function initHelpers() {
     helpers = [
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ];
 }
 function markMovesFrom() {
@@ -35,20 +48,47 @@ function markMovesFrom() {
         }
     }
 }
+function markMovesTo() {
+    initHelpers();
+    for (let x = 0; x <= 7; x++) {
+        for (let y = 0; y <= 7; y++) {
+            if (canMoveTo(x, y)) {
+                helpers[x][y] = 2;
+            }
+        }
+    }
+}
 function canMoveFrom(x, y) {
-    if (getColor(x, y) == moveColor) {
+    return getColor(x, y) == moveColor;
+}
+function canMoveTo(x, y) {
+    if (map[x][y] == ' ') {
         return true;
     } else {
-        return false;
+        return getColor(x, y) != moveColor;
     }
 }
 function getColor(x, y) {
     let figure = map[x][y];
-    if (figure == '') {
+    if (figure == ' ') {
         return '';
     }
     return figure.toUpperCase() == figure ? 'white' : 'black';
 }
+
+function clickCellFrom(x, y) {
+    moveFromX = x;
+    moveFromY = y;
+    markMovesTo();
+    showMap();
+}
+
+function clickCellTo(x, y) {
+    map[x][y] = map[moveFromX][moveFromY];
+    map[moveFromX][moveFromY] = '';
+    showMap();
+}
+
 function figureToHtml(figure) {
     switch (figure) {
         case 'K':
@@ -82,17 +122,17 @@ function figureToHtml(figure) {
 
 function showMap() {
     let color,
-        html = '<table border="1">';
+        html = '<table>';
     for (let i = 7; i >= 0; i--) {
         html += '<tr>';
         for (let j = 0; j <= 7; j++) {
-            if (helpers[i][j] == '') {
-                color = (j + i) % 2 ? '#eeffee' : '#abcdef';
+            if (helpers[j][i] == ' ') {
+                color = (j + i) % 2 ? '#E1E3E8' : '#7C8596';
             } else {
-                color = helpers[i][j] == 1 ? '#aaffaa' : '#ffaaaa';
+                color = helpers[j][i] == 1 ? '#aaffaa' : '#ffaaaa';
             }
-            html += `<td style="background-color: ${color}" id="${j}${i + 1}">`;
-            html += figureToHtml(map[i][j]);
+            html += `<td style="background-color: ${color}" id="${j}${i}">`;
+            html += figureToHtml(map[j][i]);
             html += `</td>`;
 
             if (j == 7) {
@@ -113,20 +153,36 @@ function showMap() {
     }
 
     document.querySelector('#board').innerHTML = html;
+    document.querySelectorAll('td').forEach((item) => {
+        item.addEventListener('click', (e) => {
+            var td = e.target;
+            while (td !== this && !td.matches('td')) {
+                td = td.parentNode;
+            }
+            if (td === this) {
+                console.log('No table cell found');
+            } else {
+                console.log(td.innerHTML);
+                console.log(td.id);
+                let x = td.id[0],
+                    y = td.id[1];
+                console.log(x + '' + y);
+                helpers = helpers.reverse();
+
+                if (helpers[x][y] == '1') {
+                    console.log(1);
+                    clickCellFrom(x, y);
+                }
+                if (helpers[x][y] == '2') {
+                    console.log('2');
+                    clickCellTo(x, y);
+                } else {
+                    return;
+                }
+            }
+        });
+    });
 }
 initMap();
 markMovesFrom();
 showMap();
-
-document.querySelector('table').addEventListener('click', function (event) {
-    var td = event.target;
-    while (td !== this && !td.matches('td')) {
-        td = td.parentNode;
-    }
-    if (td === this) {
-        console.log('No table cell found');
-    } else {
-        console.log(td.innerHTML);
-        console.log(td.id);
-    }
-});
