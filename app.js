72da76513f240b3a036e8f2585616bed660a8a44
Anton Liverant <anliberant@gmail.com>
2020-10-05 21:54:22 +0300

    some movements

--- app.js
+++ app.js
@@ -40,10 +40,14 @@ function initHelpers() {
 }
 function markMovesFrom() {
     initHelpers();
-    for (let x = 0; x <= 7; x++) {
-        for (let y = 0; y <= 7; y++) {
-            if (canMoveFrom(x, y)) {
-                helpers[x][y] = 1;
+    for (let sx = 0; sx <= 7; sx++) {
+        for (let sy = 0; sy <= 7; sy++) {
+            for (let dx = 0; dx <= 7; dx++) {
+                for (let dy = 0; dy <= 7; dy++) {
+                    if (canFigureMove(sx, sy, dx, dy)) {
+                        helpers[sx][sy] = 1;
+                    }
+                }
             }
         }
     }
@@ -52,7 +56,7 @@ function markMovesTo() {
     initHelpers();
     for (let x = 0; x <= 7; x++) {
         for (let y = 0; y <= 7; y++) {
-            if (canMoveTo(x, y)) {
+            if (canFigureMove(moveFromX, moveFromY, x, y)) {
                 helpers[x][y] = 2;
             }
         }
@@ -83,10 +87,119 @@ function clickCellFrom(x, y) {
     showMap();
 }
 
+function canFigureMove(sx, sy, dx, dy) {
+    if (!canMoveFrom(sx, sy)) {
+        return false;
+    }
+    if (!canMoveTo(dx, dy)) {
+        return false;
+    }
+    if (!isCorrectMove(sx, sy, dx, dy)) {
+        return false;
+    }
+    return true;
+}
+
+function isCorrectMove(sx, sy, dx, dy) {
+    let figure = map[sx][sy];
+    if (isKing(figure)) {
+        return isCorrectKingMove(sx, sy, dx, dy);
+    }
+    if (isQueen(figure)) {
+        return isCorrectQueenMove(sx, sy, dx, dy);
+    }
+    if (isBishop(figure)) {
+        return isCorrectBishopMove(sx, sy, dx, dy);
+    }
+    if (isKnight(figure)) {
+        return isCorrectKnightMove(sx, sy, dx, dy);
+    }
+    if (isRook(figure)) {
+        return isCorrectRookMove(sx, sy, dx, dy);
+    }
+    if (isPawn(figure)) {
+        return isCorrectPawnMove(sx, sy, dx, dy);
+    }
+    return true;
+}
+function isKing(figure) {
+    return figure.toUpperCase() == 'K';
+}
+function isQueen(figure) {
+    return figure.toUpperCase() == 'Q';
+}
+function isBishop(figure) {
+    return figure.toUpperCase() == 'B';
+}
+function isKnight(figure) {
+    return figure.toUpperCase() == 'N';
+}
+function isRook(figure) {
+    return figure.toUpperCase() == 'R';
+}
+function isPawn(figure) {
+    return figure.toUpperCase() == 'P';
+}
+
+function isCorrectKingMove(sx, sy, dx, dy) {
+    if (Math.abs(dx - sx) <= 1 && Math.abs(dy - sy) <= 1) {
+        return true;
+    }
+    return false;
+}
+function isCorrectQueenMove(sx, sy, dx, dy) {
+    return true;
+}
+function isCorrectBishopMove(sx, sy, dx, dy) {
+    return true;
+}
+function isCorrectKnightMove(sx, sy, dx, dy) {
+    if (Math.abs(dx - sx) == 1 && Math.abs(dy - sy) == 2) {
+        return true;
+    }
+    if (Math.abs(dx - sx) == 2 && Math.abs(dy - sy) == 1) {
+        return true;
+    }
+    return false;
+}
+function isCorrectRookMove(sx, sy, dx, dy) {
+    let deltaX = Math.sign(dx - sx);
+    let deltaY = Math.sign(dy - sy);
+
+    if (Math.abs(deltaX) + Math.abs(deltaY) != 1) {
+        return false;
+    }
+    console.log(deltaX);
+    console.log(deltaY);
+    do {
+        sx = Math.floor(sx) + Math.floor(deltaX);
+        sy = Math.floor(sy) + Math.floor(deltaY);
+        if (sx == dx && sy == dy) {
+            return true;
+        }
+        if (map[sx][sy] != ' ') {
+            return false;
+        }
+    } while (onMap(sx, sy));
+
+    return true;
+}
+function onMap(x, y) {
+    return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+}
+function isCorrectPawnMove(sx, sy, dx, dy) {
+    return true;
+}
+
 function clickCellTo(x, y) {
     map[x][y] = map[moveFromX][moveFromY];
-    map[moveFromX][moveFromY] = '';
+    map[moveFromX][moveFromY] = ' ';
+    turnMove();
+    markMovesFrom();
     showMap();
+}
+function turnMove() {
+    moveColor = moveColor == 'white' ? 'black' : 'white';
 }
 
 function figureToHtml(figure) {
@@ -162,19 +275,14 @@ function showMap() {
             if (td === this) {
                 console.log('No table cell found');
             } else {
-                console.log(td.innerHTML);
-                console.log(td.id);
                 let x = td.id[0],
                     y = td.id[1];
-                console.log(x + '' + y);
-                helpers = helpers.reverse();
+                //  helpers = helpers.reverse();
 
                 if (helpers[x][y] == '1') {
-                    console.log(1);
                     clickCellFrom(x, y);
                 }
                 if (helpers[x][y] == '2') {
-                    console.log('2');
                     clickCellTo(x, y);
                 } else {
                     return;
@@ -183,6 +291,9 @@ function showMap() {
         });
     });
 }
-initMap();
-markMovesFrom();
-showMap();
+function start() {
+    initMap();
+    markMovesFrom();
+    showMap();
+}
+start();
