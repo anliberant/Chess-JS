084814890d3a673f22e3c74285f96f2a5ef56281
Anton Liverant <anliberant@gmail.com>
2020-10-06 19:47:44 +0300

    pawn moves

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ let map = Array(),
     helpers = Array(),
     moveColor = 'white',
     moveFromX,
-    moveFromY;
+    moveFromY,
+    pawnAttackX,
+    pawnAttackY,
+    fromFigure,
+    toFigure;
 function initMap() {
     map2 = [
         ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
@@ -63,9 +67,15 @@ function markMovesTo() {
     }
 }
 function canMoveFrom(x, y) {
+    if (!onMap(x, y)) {
+        return false;
+    }
     return getColor(x, y) == moveColor;
 }
 function canMoveTo(x, y) {
+    if (!onMap(x, y)) {
+        return false;
+    }
     if (map[x][y] == ' ') {
         return true;
     } else {
@@ -80,13 +90,6 @@ function getColor(x, y) {
     return figure.toUpperCase() == figure ? 'white' : 'black';
 }
 
-function clickCellFrom(x, y) {
-    moveFromX = x;
-    moveFromY = y;
-    markMovesTo();
-    showMap();
-}
-
 function canFigureMove(sx, sy, dx, dy) {
     if (!canMoveFrom(sx, sy)) {
         return false;
@@ -94,10 +97,8 @@ function canFigureMove(sx, sy, dx, dy) {
     if (!canMoveTo(dx, dy)) {
         return false;
     }
-    if (!isCorrectMove(sx, sy, dx, dy)) {
-        return false;
-    }
-    return true;
+
+    return isCorrectMove(sx, sy, dx, dy);
 }
 
 function isCorrectMove(sx, sy, dx, dy) {
@@ -148,10 +149,37 @@ function isCorrectKingMove(sx, sy, dx, dy) {
     return false;
 }
 function isCorrectQueenMove(sx, sy, dx, dy) {
-    return true;
+    let deltaX = Math.sign(dx - sx);
+    let deltaY = Math.sign(dy - sy);
+
+    do {
+        sx = Math.floor(sx) + Math.floor(deltaX);
+        sy = Math.floor(sy) + Math.floor(deltaY);
+
+        if (sx == dx && sy == dy) {
+            return true;
+        }
+    } while (isEmpty(sx, sy));
+
+    return false;
 }
 function isCorrectBishopMove(sx, sy, dx, dy) {
-    return true;
+    let deltaX = Math.sign(dx - sx);
+    let deltaY = Math.sign(dy - sy);
+
+    if (Math.abs(deltaX) + Math.abs(deltaY) != 2) {
+        return false;
+    }
+    do {
+        sx = Math.floor(sx) + Math.floor(deltaX);
+        sy = Math.floor(sy) + Math.floor(deltaY);
+
+        if (sx == dx && sy == dy) {
+            return true;
+        }
+    } while (isEmpty(sx, sy));
+
+    return false;
 }
 function isCorrectKnightMove(sx, sy, dx, dy) {
     if (Math.abs(dx - sx) == 1 && Math.abs(dy - sy) == 2) {
@@ -169,34 +197,125 @@ function isCorrectRookMove(sx, sy, dx, dy) {
     if (Math.abs(deltaX) + Math.abs(deltaY) != 1) {
         return false;
     }
-    console.log(deltaX);
-    console.log(deltaY);
     do {
         sx = Math.floor(sx) + Math.floor(deltaX);
         sy = Math.floor(sy) + Math.floor(deltaY);
         if (sx == dx && sy == dy) {
             return true;
         }
-        if (map[sx][sy] != ' ') {
-            return false;
-        }
-    } while (onMap(sx, sy));
+    } while (isEmpty(sx, sy));
 
-    return true;
+    return false;
+}
+
+function isEmpty(x, y) {
+    if (!onMap(x, y)) {
+        return false;
+    }
+    return map[x][y] == ' ';
 }
 function onMap(x, y) {
     return x >= 0 && x <= 7 && y >= 0 && y <= 7;
 }
 function isCorrectPawnMove(sx, sy, dx, dy) {
+    if (sy < 1 || sy > 6) {
+        return false;
+    }
+    if (getColor(sx, sy) == 'white') {
+        return isCorrectSignPawnMove(sx, sy, dx, dy, +1);
+    } else if (getColor(sx, sy) == 'black') {
+        return isCorrectSignPawnMove(sx, sy, dx, dy, -1);
+    } else {
+        return false;
+    }
+}
+function isCorrectSignPawnMove(sx, sy, dx, dy, sign) {
+    if (isPawnPassant(sx, sy, dx, dy, sign)) {
+        return true;
+    }
+    if (!isEmpty(dx, dy)) {
+        if (Math.abs(dx - sx) != 1) {
+            return false;
+        }
+        return dy - sy == sign;
+    }
+    if (dx != sx) {
+        return false;
+    }
+    if (dy - sy == sign) {
+        return true;
+    }
+    if (dy - sy == sign * 2) {
+        if (sy != 1 && sy != 6) {
+            return false;
+        }
+        //return isEmpty(sx, sy + sign);
+        return true;
+    }
+
+    return false;
+}
+
+function isPawnPassant(sx, sy, dx, dy, sign) {
+    if (!(dx == pawnAttackX && dy == pawnAttackY)) {
+        return false;
+    }
+    if (sign == 1 && sy != 4) {
+        return false;
+    }
+    if (sign == -1 && sy != 3) {
+        return false;
+    }
+
+    if (dy - sy != sign) {
+        return false;
+    }
+    // if (Math.abs(dy - sy) != sign) {
+    //     return false;
+    // }
+    //return Math.abs(dx - sy) == 1;
     return true;
 }
 
-function clickCellTo(x, y) {
-    map[x][y] = map[moveFromX][moveFromY];
+function clickCellFrom(x, y) {
+    moveFromX = x;
+    moveFromY = y;
+    markMovesTo();
+    showMap();
+}
+
+function clickCellTo(toX, toY) {
+    fromFigure = map[moveFromX][moveFromY];
+    toFigure = map[toX][toY];
+
+    map[toX][toY] = fromFigure;
     map[moveFromX][moveFromY] = ' ';
+
+    checkPawnAttack(fromFigure, toX, toY);
+
     turnMove();
     markMovesFrom();
     showMap();
+}
+
+function checkPawnAttack(fromFigure, toX, toY) {
+    if (isPawn(fromFigure)) {
+        if (toX == pawnAttackX && toY == pawnAttackY) {
+            if (getColor(toX, toY) == 'white') {
+                map[toX][toY - 1] = ' ';
+            } else {
+                map[toX][Math.abs(toY) + 1] = ' ';
+            }
+        }
+    }
+    pawnAttackX = -1;
+    pawnAttackY = -1;
+    if (isPawn(fromFigure)) {
+        if (Math.abs(toY - moveFromY)) {
+            pawnAttackX = moveFromX;
+            pawnAttackY = (Math.abs(moveFromY) + Math.abs(toY)) / 2;
+        }
+    }
 }
 function turnMove() {
     moveColor = moveColor == 'white' ? 'black' : 'white';
